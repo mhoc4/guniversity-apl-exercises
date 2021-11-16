c61f4d276d7a2741f9e5d351105c7c4998bd8bcd
mhoc4 <acmjpf@gmail.com>
2021-11-16 20:30:19 -0300

    solved exercise S06E02

---
+++ Java/src/br.com.geekuniversity.com.section06/S06e01.java
@@ -0,0 +1,24 @@
+package br.com.geekuniversity.com.section06;
+
+import java.util.Scanner;
+
+public class S06e01 {
+    public static void main(String[] args) {
+        float number;
+
+        Scanner userInput =  new Scanner(System.in);
+        
+        System.out.println("Please enter a number: ");
+        number = userInput.nextFloat();
+        
+        if (number > 100) {
+            System.out.printf("\nThe number %.2f is above 100.\n", number);
+        } else {
+            number = 0;
+        System.out.printf("\n%.0f", number);
+        }
+        
+        userInput.close();
+
+    }
+}
---
+++ Java/src/br.com.geekuniversity.com.section06/S06e02.java
@@ -0,0 +1,27 @@
+package br.com.geekuniversity.com.section06;
+
+import java.util.Scanner;
+
+public class S06e02 {
+    public static void main(String[] args) {
+        float number, a , b;
+
+        Scanner userInput =  new Scanner(System.in);
+        
+        System.out.println("Please enter a number: ");
+        number = userInput.nextFloat();
+        
+        if (number > 0) {
+            a = number;
+            System.out.printf("\nThe number %.2f is positive.\n", a);
+        } else if (number < 0) {
+            b = number;
+        System.out.printf("\nThe number %.2f is negative", b);
+        } else {
+            System.out.printf("You entered the number %.0f.", number);
+        }
+        
+        userInput.close();
+
+    }
+}
