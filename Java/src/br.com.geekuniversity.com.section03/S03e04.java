d229128cf0f1f0507040bfe7ca5b63e7a3caa7de
mhoc4 <acmjpf@gmail.com>
2021-11-16 19:46:37 -0300

    solved exercise S03E04

---
+++ Java/src/br.com.geekuniversity.com.section03/S03e04.java
@@ -0,0 +1,24 @@
+package br.com.geekuniversity.com.section03;
+
+import java.util.Scanner;
+
+public class S03e04 {
+    public static void main(String[] args) {
+        float numberOne, numberTwo, result;
+
+        Scanner userInput =  new Scanner(System.in);
+        
+        System.out.println("Please enter the first number: ");
+        numberOne = userInput.nextFloat();
+        
+        System.out.println("Please enter the second number: ");
+        numberTwo = userInput.nextFloat();
+
+        result = numberOne + numberTwo;
+
+        System.out.println("\nThe sum result is: " + result);
+        
+        userInput.close();
+
+    }
+}
