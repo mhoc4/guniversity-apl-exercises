21c3aff4939a4160d958886af36348ca8fc4ea98
mhoc4 <acmjpf@gmail.com>
2021-11-16 20:41:37 -0300

    solved exercise S06E03

---
+++ Java/src/br.com.geekuniversity.com.section06/S06e03.java
@@ -0,0 +1,26 @@
+package br.com.geekuniversity.com.section06;
+
+import java.util.Scanner;
+
+public class S06e03 {
+    public static void main(String[] args) {
+        float number, p = 0, i = 0;
+
+        Scanner userInput =  new Scanner(System.in);
+        
+        System.out.println("Please enter a number: ");
+        number = userInput.nextFloat();
+        
+        if (number % 2 == 0) {
+            p = number;
+        } else {
+            i = number;
+        }
+
+        System.out.println("\n"+ p);
+        System.out.println(i);
+        
+        userInput.close();
+
+    }
+}
