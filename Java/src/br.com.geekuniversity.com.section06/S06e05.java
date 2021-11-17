c9160c4398586ed743946fcc26e8095803539c1e
mhoc4 <acmjpf@gmail.com>
2021-11-17 14:44:21 -0300

    solved exercise S06E05

---
+++ Java/src/br.com.geekuniversity.com.section06/S06e05.java
@@ -0,0 +1,29 @@
+package br.com.geekuniversity.com.section06;
+
+import java.util.Scanner;
+
+public class S06e05 {
+    public static void main(String[] args) {
+        float p, e, m, weightlimit = 50.00F;
+        int fineValuePerKg = 4;   
+
+        Scanner userInput =  new Scanner(System.in);
+        
+        System.out.println("Please enter the total fish caught weight: ");
+        p = userInput.nextFloat();
+
+        if (p > weightlimit) {
+            e = p - weightlimit;
+            m = e * fineValuePerKg;
+            
+        } else {
+            e = 0;
+            m = 0;
+        }
+
+        System.out.printf("\nFish caught weight: %.2f\nAbove weight limit: %.2f\nFine: %.2f\n", p, e, m);
+        
+        userInput.close();
+
+    }
+}
