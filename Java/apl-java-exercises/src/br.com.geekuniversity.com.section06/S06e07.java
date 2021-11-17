26f802f2d3a175cf9a356d35facdc81ab7bf1a92
mhoc4 <acmjpf@gmail.com>
2021-11-17 16:06:46 -0300

    solved exercise S06E07

---
+++ Java/apl-java-exercises/src/br.com.geekuniversity.com.section06/S06e07.java
@@ -0,0 +1,28 @@
+package br.com.geekuniversity.com.section06;
+
+import java.util.Scanner;
+
+public class S06e07 {
+    public static void main(String[] args) {
+        float numbers[] = new float[4], numbersSquares[] = new float[4];
+
+        Scanner userInput =  new Scanner(System.in);
+        
+        for (int counter = 0; counter < 4; counter++) {
+        System.out.printf("Please enter a number for index %d: ", counter);
+        numbers[counter] = userInput.nextFloat();
+        numbersSquares[counter] = numbers[counter] * numbers[counter];
+        }
+        
+        if (numbersSquares[2] > 1000) {
+            System.out.printf("\n%.2f", numbersSquares[2]);
+        } else {
+            for (int counter = 0; counter < 4; counter++) {
+            System.out.printf("\nNumber: %.2f Number Square: %.2f", numbers[counter], numbersSquares[counter]);
+            }
+        }
+        
+        userInput.close();
+
+    }
+}
