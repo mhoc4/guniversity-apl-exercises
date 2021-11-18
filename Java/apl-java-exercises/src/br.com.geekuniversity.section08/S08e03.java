dc07d42f4ebf3a4eeeff8c1ed44b901e85a6ad3f
mhoc4 <acmjpf@gmail.com>
2021-11-18 18:00:52 -0300

    solved exercise S08e03

---
+++ Java/apl-java-exercises/src/br.com.geekuniversity.section08/S08e03.java
@@ -0,0 +1,25 @@
+package br.com.geekuniversity.section08;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class S08e03 {
+    
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        Scanner userInput = new Scanner(System.in);
+
+        for (int counter = 0; counter < 10; counter++){
+        
+            System.out.printf("Please enter a value for the number at index %d: \n", counter);
+            numbers.add(userInput.nextInt());
+        }
+        
+        for (int counter = numbers.size()-1; counter > 0;counter--) {
+            System.out.printf("\nNumber at index %d: %d", counter, numbers.get(counter));
+        }
+
+        userInput.close();
+    }
+}
