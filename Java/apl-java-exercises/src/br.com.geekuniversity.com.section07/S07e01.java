0625c3ea48cd7930aa8420f3e542e2577f7944b5
mhoc4 <acmjpf@gmail.com>
2021-11-18 10:20:53 -0300

    solved exercise S07e01

---
+++ Java/apl-java-exercises/src/br.com.geekuniversity.com.section07/S07e01.java
@@ -0,0 +1,32 @@
+package br.com.geekuniversity.com.section07;
+
+import java.util.Scanner;
+
+public class S07e01 {  
+        
+    public static void main(String[] args) {
+        int swimmerAge;
+        String category = "not in a category";
+        
+        Scanner userInput =  new Scanner(System.in);
+        
+        System.out.println("Please enter an integer number: ");
+        swimmerAge = userInput.nextInt();
+
+        if (swimmerAge >= 18) {
+            category = "Adult";
+        } else if (swimmerAge >= 14) {
+            category = "juvenile B";
+        } else if (swimmerAge >= 12) {
+            category = "juvenile A";
+        } else if (swimmerAge >= 8) {
+            category = "child B";
+        } else if (swimmerAge >= 5) {
+            category = "child A";
+        } 
+        System.out.printf("\n%s", category);
+
+        userInput.close();
+
+    }
+}
