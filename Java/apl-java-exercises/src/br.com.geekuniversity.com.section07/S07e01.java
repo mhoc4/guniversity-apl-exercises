38825257d6ac6173695ecbcceb282eb64c5d85f2
mhoc4 <acmjpf@gmail.com>
2021-11-18 10:53:06 -0300

    S07e01 now solved

--- Java/apl-java-exercises/src/br.com.geekuniversity.com.section07/S07e01.java
+++ Java/apl-java-exercises/src/br.com.geekuniversity.com.section07/S07e01.java
@@ -5,26 +5,20 @@ import java.util.Scanner;
 public class S07e01 {  
         
     public static void main(String[] args) {
-        int swimmerAge;
-        String category = "not in a category";
+        double numberInput = 1, biggestNumber = 0;
         
         Scanner userInput =  new Scanner(System.in);
         
-        System.out.println("Please enter an integer number: ");
-        swimmerAge = userInput.nextInt();
+        while(numberInput != 0) {
+            System.out.println("Please enter a number: ");
+            numberInput = userInput.nextDouble();
+            
 
-        if (swimmerAge >= 18) {
-            category = "Adult";
-        } else if (swimmerAge >= 14) {
-            category = "juvenile B";
-        } else if (swimmerAge >= 12) {
-            category = "juvenile A";
-        } else if (swimmerAge >= 8) {
-            category = "child B";
-        } else if (swimmerAge >= 5) {
-            category = "child A";
-        } 
-        System.out.printf("\n%s", category);
+            if (numberInput > biggestNumber) {
+            biggestNumber = numberInput;
+            }
+        }
+        System.out.printf("The biggest number entered is: %.2f", biggestNumber);
 
         userInput.close();
 
