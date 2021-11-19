d012c600929f86b81c92ac31d204cf3554a22526
mhoc4 <acmjpf@gmail.com>
2021-11-19 09:42:41 -0300

    few changes

--- Python/section03/S03e07.py
+++ Python/section03/S03e07.py
@@ -2,4 +2,4 @@ personHeight = float(input ("How tall are you in meters? "))
 
 idealBMI = (72.7 * personHeight - 58)
 
-print("\nYour ideal BMI is: {0:.2f}".format(idealBMI))
+print("Your ideal BMI is: {0:.2f}".format(idealBMI))
