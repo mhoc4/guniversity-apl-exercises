639d8a76cd75f6ece6241489a9816a3ebd016116
mhoc4 <acmjpf@gmail.com>
2021-11-19 10:06:19 -0300

    solved exercise S06e04

---
+++ Python/section06/S06e04.py
@@ -0,0 +1,12 @@
+personHeight = float(input ("How tall are you in meters? "))
+personGender = input("Please enter your gender f/m.")
+
+if personGender.lower() == 'm':
+    idealBMI = (72.7 * personHeight - 58)
+elif personGender.lower() == 'f':
+    idealBMI = (62.1 * personHeight) - 44.7
+else:
+    idealBMI = 0
+    print("\nGender not found.")
+
+print("\nYour ideal BMI is: {0:.2f}".format(idealBMI))
