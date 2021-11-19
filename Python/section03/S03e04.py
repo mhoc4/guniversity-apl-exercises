9b689ee9e63452fb993ea6f72e4e56ce19e413fd
mhoc4 <acmjpf@gmail.com>
2021-11-19 09:10:34 -0300

    solved exercise S03e04

---
+++ Python/section03/S03e04.py
@@ -0,0 +1,5 @@
+numberOne = int(input ("Please enter the value in meters: "))
+
+centimeters = numberOne * 100
+
+print("The value {0} meters in centimeters is: {1} centimeters".format(numberOne, centimeters))
