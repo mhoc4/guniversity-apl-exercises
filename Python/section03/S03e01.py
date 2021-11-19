bbdea97716e6730c1bb2f237e6fa5d6c57e599e8
mhoc4 <acmjpf@gmail.com>
2021-11-19 09:11:54 -0300

    wrong exercise number

--- Python/section03/S03e01.py
+++
@@ -1,6 +0,0 @@
-minimumStock = int(input ("Please enter the first number: "))
-maximumStock = int(input ("Please enter the second number: "))
-
-averageStock = (minimumStock + maximumStock) / 2
-
-print("The average stock is: {0}".format(averageStock))
