c6abb079ebc299c4630e2495acb9b4c612c1f52e
mhoc4 <acmjpf@gmail.com>
2021-11-19 10:05:45 -0300

    solved exercise S06e02

---
+++ Python/section06/S06e02.py
@@ -0,0 +1,8 @@
+number = int(input("Please enter a number: "))
+
+if number > 0:
+    a = number
+    print("\nPositive number: {0}".format(a))
+else:
+    b = number
+    print("\nNegative number: {0}".format(b))
