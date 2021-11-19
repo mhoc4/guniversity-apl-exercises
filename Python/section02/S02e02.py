38e1ca92c4d05ab582e8bd70fcb32a3a390b7c90
mhoc4 <acmjpf@gmail.com>
2021-11-19 02:10:36 -0300

    solved exercise S02e02

---
+++ Python/section02/S02e02.py
@@ -0,0 +1,5 @@
+num1 = int(input ("Please enter the first number: "))
+num2 = int(input ("Please enter the second number: "))
+sum = num1 + num2
+mult = sum * num1
+print("The result of the multiplication is: {0}".format(mult))
