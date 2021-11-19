cb42b14eab70eebe38e7b0e144f0aa4261a5e948
mhoc4 <acmjpf@gmail.com>
2021-11-19 13:54:16 -0300

    solved exercise S07e07

---
+++ Python/section07/S07e07.py
@@ -0,0 +1,46 @@
+mouseQuantity = 0
+needsSphere = 0
+needsCleaning = 0
+needsConectorOrCableChange = 0
+brokenOrUseless = 0
+needsSpherePercentage = 0
+needsCleaningPercentage = 0
+needsConectorOrCableChangePercentage = 0
+brokenOruselessPercentage = 0
+
+mouseCode = int(input("\nPlease enter a mouse code, enter 0 to show results: "))
+
+if mouseCode != 0:
+    while mouseCode != 0:
+        
+        optionCode = int(input("\nPlease choose an option:\n1 - needs sphere.\n2 - needs cleaning\n3 - needs conector or cable change\n4 - broken or useless\n"))
+        
+        if optionCode > 1 or optionCode < 4:
+            while optionCode < 1 or optionCode > 4:
+                optionCode = int(input("\nNo such code, please choose between 1 and 4: \n1 - needs sphere.\n2 - needs cleaning\n3 - needs conector or cable change\n4 - broken or useless\n"))
+     
+        if optionCode == 1:
+            needsSphere += 1
+        elif optionCode == 2:
+            needsCleaning += 1
+        elif optionCode == 3:
+            needsConectorOrCableChange += 1
+        else:
+            brokenOrUseless += 1
+        
+        mouseQuantity += 1
+        print("\nMouse status saved.\n")
+        mouseCode = int(input("\nPlease enter a mouse code, enter 0 to show results: "))
+
+    if mouseQuantity != 0:
+        needsSpherePercentage = (needsSphere / mouseQuantity) * 100
+        needsCleaningPercentage = (needsCleaning / mouseQuantity) * 100
+        needsConectorOrCableChangePercentage = (needsConectorOrCableChange / mouseQuantity) * 100
+        brokenOruselessPercentage = (brokenOrUseless / mouseQuantity) * 100
+
+print("\nMouse quantity: {0}\n".format(mouseQuantity))
+print("Status\t\t\t\t\tQuantity\t\tPercentage")
+print("1 - needs sphere\t\t\t{0}\t\t\t{1}%".format(needsSphere, needsSpherePercentage))
+print("2 - needs cleaning\t\t\t{0}\t\t\t{1}%".format(needsCleaning, needsCleaningPercentage))
+print("3 - needs conect or cable change\t{0}\t\t\t{1}%".format(needsConectorOrCableChange, needsConectorOrCableChangePercentage))
+print("4 - broken or useless\t\t\t{0}\t\t\t{1}%".format(brokenOrUseless, brokenOruselessPercentage))
