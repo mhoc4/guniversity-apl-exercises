3aafedfef7555fdfb5f6c3b35870fb0cd129102b
mhoc4 <acmjpf@gmail.com>
2021-11-19 12:40:31 -0300

    fixed a typo

--- Python/section07/S07e05.py
+++ Python/section07/S07e05.py
@@ -2,7 +2,7 @@ login = input("\nPlease enter your login: ")
 password = input("Please enter your password: ")
 if password == login:
     while login == password:
-        print("\nYour password can't be identical than your password.\nPlease try again.")
+        print("\nYour password must be different than your login.\nPlease try again.")
         login = input("\nPlease enter your login: ")
         password = input("Please enter your password: ")
 print("\nYou have succesfully logged in.")
